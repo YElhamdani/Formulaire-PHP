590011d1ab7938cd2e6267a3eb0914a086bd8017
Younes <48552550+YElhamdani@users.noreply.github.com>
2019-04-22 14:20:36 +0200

    Add files via upload

--- Formulaire-PHP/test.php
+++ Formulaire-PHP/test.php
@@ -1,8 +1,17 @@
-<?php 
-    
-    $Var1 = '10';
-    $Var2 = '23';
-    $result = $Var1 + $Var2;
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $version = 7;
+        
+        echo "hi, me it's Younes i descovery the PHP version $version"
 
-    echo "$result";
-?>
+    ?>
+</body>
+</html>
